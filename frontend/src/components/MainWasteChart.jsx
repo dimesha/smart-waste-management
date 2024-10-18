c8db214a92b745254c7c7b181b9ae29f0c7f2f59
Pasan-FDo <pasanfernando.work@gmail.com>
2024-10-18 23:28:57 +0530

    report part completd

--- frontend/src/components/MainWasteChart.jsx
+++ frontend/src/components/MainWasteChart.jsx
@@ -6,12 +6,12 @@ const MainWasteChart = ({ data }) => {
   
   return (
     <div className='w-full h-auto'>
-      <h2>Waste Report</h2>
-      <div style={{ width: '50%', height: 400 }}> {/* Set a fixed height */}
+      <h2 className='text-center text-3xl font-bold py-5 text-green-400 '>Waste Report</h2>
+      <div style={{ width: '90%', height: 400, paddingLeft:'5%'}}> {/* Set a fixed height */}
         <ResponsiveContainer width="100%" height="100%">
           <BarChart data={data}>
             <CartesianGrid strokeDasharray="3 3" />
-            <XAxis dataKey="wasteType" />
+            <XAxis dataKey="wasteType" />kk
             <YAxis />
             <Tooltip />
             <Legend />
